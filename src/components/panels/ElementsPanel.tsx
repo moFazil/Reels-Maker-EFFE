ee41d5123c430d9d1a722820c9efee6ca0d1d08c
moFazil <fazelcm2000@gmail.com>
2023-12-11 16:51:26 +0530

    Merge Update

--- src/components/panels/ElementsPanel.tsx
+++ src/components/panels/ElementsPanel.tsx
@@ -7,15 +7,9 @@ import { Element } from "../entity/Element";
 export const ElementsPanel = observer((_props: {}) => {
   const store = React.useContext(StoreContext);
   return (
-<<<<<<< HEAD
-    <div className=" overflow-y-auto max-h-full">
-      <div className="flex flex-row justify-between">
-        <div className="text-xl px-9 pt-5 font-semibold">Elements</div>
-=======
     <div className="overflow-y-auto max-h-full">
       <div className="flex flex-row justify-between">
         <div className="text-xl font-semibold px-9 pt-5">Elements</div>
->>>>>>> 9147fab5b1844a64103bbfa6425da56472f261d4
       </div>
       <div className="flex flex-col px-5 pt-2">
         {store.editorElements.map((element) => {
