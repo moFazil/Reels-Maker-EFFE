9147fab5b1844a64103bbfa6425da56472f261d4
ahamed001 <ahamed001@gmail.com>
2023-12-11 16:09:57 +0530

    Elements Scroll Added

--- src/components/panels/ElementsPanel.tsx
+++ src/components/panels/ElementsPanel.tsx
@@ -7,11 +7,11 @@ import { Element } from "../entity/Element";
 export const ElementsPanel = observer((_props: {}) => {
   const store = React.useContext(StoreContext);
   return (
-    <div>
+    <div className="overflow-y-auto max-h-full">
       <div className="flex flex-row justify-between">
-        <div className="text-sm px-[16px] py-[7px] font-semibold">Elements</div>
+        <div className="text-xl font-semibold px-9 pt-5">Elements</div>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col px-5 pt-2">
         {store.editorElements.map((element) => {
           return <Element key={element.id} element={element} />;
         })}
