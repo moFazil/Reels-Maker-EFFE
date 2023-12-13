ed15652ee81f2e9d4cfd680dcf3349f90a96d9e1
moFazil <fazelcm2000@gmail.com>
2023-12-13 10:48:30 +0530

    Add

--- src/components/panels/TextResourcesPanel.tsx
+++ src/components/panels/TextResourcesPanel.tsx
@@ -1,12 +1,13 @@
-"use client";
-import React from "react";
-import { observer } from "mobx-react";
-import { TextResource } from "../entity/TextResource";
+// TextResourcesPanel.tsx
+
+import React from 'react';
+import { observer } from 'mobx-react';
+import { TextResource } from '../entity/TextResource';
 
 export const TextResourcesPanel = observer(() => {
   return (
     <div className="h-full">
-      <div className="text-sm px-[16px] pt-[16px] pb-[8px] font-semibold text-slate-800`">
+      <div className="text-sm px-[16px] pt-[16px] pb-[8px] font-semibold text-slate-800">
         Add Text
       </div>
       <TextResource sampleText="Title" fontSize={28} fontWeight={800} />
@@ -16,3 +17,4 @@ export const TextResourcesPanel = observer(() => {
     </div>
   );
 });
+
